ca41358bd81d1d89e839a617ce9cc93fa8895447
dima <dima@thinkpad.(none)>
2012-11-21 20:34:31 +0400

    hi

--- web.rb
+++ web.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
+set :static, "/public"
+
 get '/' do
   "Hello, world!!!! Welcome here"
 end
 
 get '/hi' do
   "Hello, world!!!! Welcome here\n\nhi page"
+end
+
+get '/test' do
+  redirect "/public/index.html"
 end
