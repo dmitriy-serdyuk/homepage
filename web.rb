a6e62ecf3c69dac9f6e4559424385599e26f4f9a
dima <dima@thinkpad.(none)>
2012-11-21 21:04:00 +0400

    hi

--- web.rb
+++ web.rb
@@ -3,13 +3,7 @@ require 'sinatra'
 set :public_folder, "/public"
 
 get '/' do
-  "Hello, world!!!! Welcome here"
+  #"Hello, world!!!! Welcome here"
+  redirect "/static/index.html"
 end
 
-get '/hi' do
-  "Hello, world!!!! Welcome here\n\nhi page"
-end
-
-get '/test' do
-  redirect "/public/index.html"
-end
